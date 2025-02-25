87ebc31c8c072df1b5b06d43ddbb2889299d782d
Jan-C-Nielsen <U2488247@rts-365.dk>
2025-02-25 09:54:41 +0100

    save

--- detail.js
+++ detail.js
@@ -7,6 +7,13 @@ console.log(url);
 
 let sectionElm = document.createElement("section")
 
+function info(text1, text2)
+{
+   return  `<div class="infobox">
+    <p>${text1}</p>
+    <p>${text2}</p>
+    </div>`;
+}
 
 fetch(url)
     .then(function (response) {
@@ -14,27 +21,42 @@ fetch(url)
     }).then(
         function (data) {
             let innerHTML = "";
-            console.log(data);
+            console.log(info(data.height, "height"));
             let divElm = document.createElement("div");
-            let id = url.slice(0, -1).split("/").pop()
-            innerHTML = `
-            <span>${data.name}</span>
-             <span>${data.id}</span>
+            let id = url.slice(0, -1).split("/").pop();
 
-            <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${id}.png" alt="">
-`
-innerHTML += data.types.map(function (t) {
-              return  `
-                 <span> ${t.type.name}</span>
+            innerHTML = `
+            <p>${data.name}</p>
+             <p>${data.id}</p>
+
+           <div> 
+           <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${id}.png" alt="">
+</div>
+           `
+            innerHTML += `<div class = "types">` + data.types.map(function (t) {
+                return `
+                 <p> ${t.type.name}</p>
              `
-        }).join("")
-        innerHTML += `
-        <span>${data.weight}</span>
-        <span>${data.height}</span>
+            }).join("") + "</div>";
+
+            innerHTML += `
+            <div class="info">
+          ${info(data.height, "height")}
+          ${info(data.weight, "weight")}
         
+        <p>${data.moves[0].move.name}</p>
+        <p>${data.moves[1].move.name}</p>
+        </div>
+
 `
-        divElm.innerHTML = innerHTML;
-        console.log("innerHTML:"+innerHTML);
+            innerHTML += data.stats.map(function (s) {
+                return `
+       <p> ${s.stat.name}</p>
+       <p> ${s.base_stat}</p>
+   `
+            }).join("")
+            divElm.innerHTML = innerHTML;
+            console.log("innerHTML:" + innerHTML);
             sectionElm.append(divElm)
         }
     )
